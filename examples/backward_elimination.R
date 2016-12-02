8dd94133c8cdcb3b0596357abb0e93f8dcd3facd
enriquea <enrique.audain@gmail.com>
2016-12-02 23:44:42 +0100

    added FS example

---
+++ examples/backward_elimination.R
@@ -0,0 +1,42 @@
+
+# Load caret library
+require(caret);
+
+# Load data features (example: Branca dataset with AAindex descriptors estimated)
+load(file = "data/brancaAAindexFeatures.rda")
+data_features <- brancaFeatures
+
+# Load data classes (example: Response variable, Branca peptides experimental variable)
+data_class <- as.matrix(read.table('data/branca_peptides.csv', header=TRUE, sep = ','))[,2]
+
+# Scale data features
+data_features<- scale(data_features, center=TRUE, scale=TRUE)
+
+# Divide the dataset in train and test sets
+inTrain <- createDataPartition(data_class, p = 3/4, list = FALSE)
+
+# Create the Training Dataset for Descriptors 
+trainDescr <- data_features[inTrain,]
+
+# Create the Testing dataset for Descriptors
+testDescr <- data_features[-inTrain,]
+
+trainClass <- data_class[inTrain]
+testClass <- data_class[-inTrain]
+
+# Here, we can included a correlation matrix analysis to remove 
+# the redundant features before to perform the backwards selection approach.
+descrCorr <- cor(trainDescr)
+highCorr <- findCorrelation(descrCorr, 0.5)
+trainDescr <- trainDescr[, -highCorr]
+testDescr <- testDescr[, -highCorr]
+
+# caret function: the rfe is the backwards selection, 
+# c is the possible sizes of the features sets, 
+# and method the optimization method is a support vector machine.
+svmProfile <- rfe(x=trainDescr, 
+                  y=trainClass, 
+                  sizes=c(1:5), 
+                  rfeControl=rfeControl(functions = caretFuncs, number = 2), 
+                  method = "svmRadial", 
+                  fit = FALSE)
