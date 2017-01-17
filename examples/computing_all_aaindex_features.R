0eb9f3679257e6d4c2919b699691d7312ef65814
Enrique Audain <enrique.audain@gmail.com>
2017-01-17 10:52:57 +0100

    minor change

--- examples/computing_all_aaindex_features.R
+++ examples/computing_all_aaindex_features.R
@@ -2,18 +2,18 @@
 
 # loading peptides file
 filepath <- system.file("extdata", 'branca_peptides.csv', package = "feseR")
-brancaDataSet <- read.table(file = filepath, header = TRUE, sep = ',', stringsAsFactors = FALSE)
+peptideDataSet <- read.table(file = filepath, header = TRUE, sep = ',', stringsAsFactors = FALSE)
 
 # retrieving aminoacid sequences
-sequence.list <- as.list(brancaDataSet$sequence)
+sequence.list <- as.list(peptideDataSet$sequence)
 
 # computing AAindex features
 temp.list <- lapply(sequence.list, function(x) as.vector(computeAllAAindexValues(x)))
 
 # convert to matrix and rename rows and cols
-brancaFeatures <- do.call(rbind, temp.list)
-rownames(brancaFeatures) <- unlist(sequence.list)
-colnames(brancaFeatures) <- rownames(aaindexMatrix)
+peptideFeatures <- do.call(rbind, temp.list)
+rownames(peptideFeatures) <- unlist(sequence.list)
+colnames(peptideFeatures) <- rownames(aaindexMatrix)
 
 # save data with all AAindex pre-computed descriptors (mean)
-save(brancaFeatures, file = 'data/brancaAAindexFeatures.rda')
+save(peptideFeatures, file = 'data/brancaAAindexFeatures.rda')
