ea767a8c850a89aeaaa3fdaeafa33f38f5be9230
enriquea <enrique.audain@gmail.com>
2021-03-23 18:59:22 +0100

    handle missingness

--- R/utils.R
+++ R/utils.R
@@ -1,5 +1,5 @@
 
-## Useful functions for checking/comparing matrices
+## Useful functions
 
 #' valid.matrix
 #'
@@ -47,6 +47,40 @@ compare.matrix <- function(input.matrix, output.matrix, description = 'Number of
   if (ncol2 < 2){
     stop('Removed all features from input matrix...')
   } else {
-    message(paste(description, round((100 - ncol2/ncol1*100), 2), '%', sep = ' '))
+    message(paste(description, round(((ncol2 - ncol1)/ncol1*100), 2), '%', sep = ' '))
   }
+}
+
+
+computeMissingRate <- function(v){
+   v <- as.numeric(v)
+   mr <- sum(is.na(v)) / length(v)
+   return(round(mr, 2))
+}
+
+
+getMissingnessRateByFeatures <- function(mx){
+  if(length(colnames(mx)) == 0) stop('Column names are missing...')
+  missingness_rate <- apply(mx, 2, computeMissingRate)
+  df <- data.frame(features=names(missingness_rate),
+                   missingness=missingness_rate)
+  return(df)
+}
+
+
+impute <- function(v, method='mean'){
+   v <- as.numeric(v)
+   if(method=='mean'){
+     v[is.na(v)] <- mean(v, na.rm = T)
+   } 
+   if(method=='median'){
+     v[is.na(v)] <- median(v, na.rm = T)
+   }
+   return(v)
+}
+
+imputeMatrix <- function(mx, method='mean'){
+   message(paste0("Imputing matrix with method: ", method))
+   mx.imputed <- apply(mx, 2, impute, method = method)
+   return(mx.imputed)
 }
